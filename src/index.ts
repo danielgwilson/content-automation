8ad20ab2e861e5a595e4357fcf947b44bb902a88
Daniel G. Wilson <daniel@danielgwilson.com>
2020-01-28 12:32:11 -0800

    Working (gets hot page)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-console.log("Hello World!");
-// hi
+import config from "config";
+import snoowrap from "snoowrap";
+
+const r = new snoowrap({
+  userAgent: config.get("REDDIT_USER_AGENT"),
+  clientId: config.get("REDDIT_CLIENT_ID"),
+  clientSecret: config.get("REDDIT_CLIENT_SECRET"),
+  refreshToken: config.get("REDDIT_REFRESH_TOKEN")
+});
+
+r.getHot().then(posts => posts.map(post => console.log(post.title)));
