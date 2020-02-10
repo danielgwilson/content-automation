f9150b0fcd915682b6f4bb43f512bf83fd27ebe8
Daniel G. Wilson <daniel@danielgwilson.com>
2020-02-09 17:41:44 -0800

    Misc. improvements

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import fs from "fs";
+import path from "path";
 import config from "config";
 import Scraper from "./scraper/scraper";
 import Generator from "./generator/generator";
 
 (async () => {
+  const outputDir = path.join(__dirname, "/temp/");
+
   const scraper = new Scraper({
     userAgent: config.get("REDDIT_USER_AGENT"),
     clientId: config.get("REDDIT_CLIENT_ID"),
@@ -10,12 +14,16 @@ import Generator from "./generator/generator";
     refreshToken: config.get("REDDIT_REFRESH_TOKEN")
   });
   const post = await scraper.getPost({
+    outputDir,
     subredditName: "AskReddit",
     postIndex: 2,
     minWords: 2.6 * 60 * 10,
     sort: "top",
     time: "week"
   });
+
+  fs.writeFileSync(path.join(outputDir, "output.json"), post);
+
   console.log(`\n----------`);
   console.log(`Title: ${post.title}`);
   console.log(`Score: ${post.score}`);
--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -27,18 +27,20 @@ export default class {
   }
 
   async getPost({
-    subredditName = "AskReddit",
+    outputDir,
+    subredditName,
     postIndex = 0,
     minWords = 3 * 600,
     sort = "hot",
     time = undefined
   }: {
-    subredditName?: string;
+    outputDir: string;
+    subredditName: string;
     postIndex?: number;
     minWords?: number;
     sort?: "hot" | "top";
     time?: "hour" | "day" | "week" | "month" | "year" | "all";
-  } = {}): Promise<IPost> {
+  }): Promise<IPost> {
     const subreddit = this.r.getSubreddit(subredditName);
     const posts =
       sort === "hot"
@@ -46,13 +48,12 @@ export default class {
         : await subreddit.getTop({ time, limit: postIndex + 1 });
     const topPost = posts[postIndex];
 
-    const targetDir = path.join(__dirname, "/../", "./temp/");
-    if (!fs.existsSync(targetDir)) {
-      fs.mkdirSync(targetDir);
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
     }
     await fetchAndSaveFile(
       await subreddit.icon_img,
-      path.join(targetDir, "subreddit-icon.png")
+      path.join(outputDir, "subreddit-icon.png")
     );
 
     const {
@@ -66,44 +67,6 @@ export default class {
       gildings
     } = topPost;
 
-    const cleanComments: {
-      author: string;
-      score: number;
-      body: string;
-      body_html: string;
-      gildings: Gildings;
-    }[] = [];
-    const commentBatchSize = 10;
-    let nWords = 0;
-    let nextComments = await comments.fetchMore({
-      amount: commentBatchSize,
-      skipReplies: true,
-      append: false
-    });
-    while (nWords < minWords) {
-      nextComments = await nextComments.fetchMore({
-        amount: commentBatchSize,
-        skipReplies: true,
-        append: false
-      });
-      for (let comment of nextComments) {
-        cleanComments.push({
-          author: comment.author.name,
-          score: comment.score,
-          body: comment.body,
-          body_html: comment.body_html,
-          gildings: comment.gildings
-        });
-
-        nWords += getWordCountForString(comment.body);
-
-        if (nWords >= minWords) break;
-      }
-    }
-    console.log(
-      getWordCountForStrings(cleanComments.map(comment => comment.body))
-    );
-
     return {
       id,
       title,
@@ -112,10 +75,48 @@ export default class {
       upvoteRatio: upvote_ratio,
       author: author.name,
       numComments: num_comments,
-      comments: cleanComments,
+      comments: await getCleanComments(comments, minWords),
       gildings
     };
   }
+}
+
+async function getCleanComments(
+  comments: snoowrap.Listing<snoowrap.Comment>,
+  minWords: number
+) {
+  const cleanComments: {
+    author: string;
+    score: number;
+    body: string;
+    body_html: string;
+    gildings: Gildings;
+  }[] = [];
+  const commentBatchSize = 10;
+  let nWords = 0;
+  let nextComments = comments;
+  while (nWords < minWords) {
+    nextComments = await nextComments.fetchMore({
+      amount: commentBatchSize,
+      skipReplies: true,
+      append: false
+    });
+    const commentBatch = nextComments.slice(cleanComments.length);
+    for (let comment of commentBatch) {
+      cleanComments.push({
+        author: comment.author.name,
+        score: comment.score,
+        body: comment.body,
+        body_html: comment.body_html,
+        gildings: comment.gildings
+      });
+
+      nWords += getWordCountForString(comment.body);
+
+      if (nWords >= minWords) break;
+    }
+  }
+  return cleanComments;
 }
 
 function getWordCountForString(str: string) {
